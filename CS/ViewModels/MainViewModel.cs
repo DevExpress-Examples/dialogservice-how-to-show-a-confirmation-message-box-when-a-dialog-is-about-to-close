de1ae77a37834b4b1d8fe102b837aa7c02ea7656
Andrey Marten <andrey.kunitsyn@devexpress.com>
2018-09-26 18:28:46 +0300

    add example content

---
+++ CS/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Mvvm;
+
+namespace dxDialog1.ViewModels {
+    public class MainViewModel : ViewModelBase {
+        protected IDialogService DialogService { get { return this.GetService<IDialogService>(); } }
+
+        public MainViewModel() {
+            ShowDialogCommand = new DelegateCommand(ShowDialog);
+        }
+
+        public DelegateCommand ShowDialogCommand { get; private set; } 
+
+        public async void ShowDialog() {
+
+            var result = await DialogService.ShowDialogAsync(
+                MessageButton.OKCancel,
+                "My Dialog Window",
+                "DialogView",
+                null,
+                this);
+
+        }
+        
+    }
+}
---
+++ CS/Views/DialogView.xaml.cs
@@ -0,0 +1,9 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace dxDialog1.Views {
+    public sealed partial class DialogView : UserControl {
+        public DialogView() {
+            this.InitializeComponent();
+        }
+    }
+}
